6033b1042a7f978892bd08aab80eebffe21bf014
jsachdeva7 <jsachdeva7@gatech.edu>
2026-05-24 12:32:14 -0400

    Initial Commit

---
+++ tmp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tmp
+//
+//  Created by Jagat Sachdeva on 5/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tmp/tmpApp.swift
@@ -0,0 +1,17 @@
+//
+//  tmpApp.swift
+//  tmp
+//
+//  Created by Jagat Sachdeva on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct tmpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
